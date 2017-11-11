fe2332804bee98763074b35b7c446c945ff0e409
yushghimire <yushghimire@gmail.com>
2017-11-11 14:36:18 +0545

    the change of ant crusher to oop

--- js-experiments/3-ant-smasher-version2/js/smash.js
+++ js-experiments/3-ant-smasher-version2/js/smash.js
@@ -1,226 +1,231 @@
-//declaration
-var main=document.getElementById("main-wrapper");
+//global declaration
 var ANT_WIDTH = 50;
 var ANT_HEIGHT = 50;
-var bodies = document.getElementsByTagName("body");
-var unOrderedList= document.createElement("ul");
-var ants = [];
-var smallBoxWidth =0;
+var MAX_BOX_WIDTH = 1000;
+var MAX_BOX_HEIGHT = 500;
+var main=document.getElementById("main-wrapper");
 var moveAnts;
 
-//create list
-unOrderedList.style.fontSize ="15px";
-
-bodies[0].appendChild(unOrderedList);
-
-main.style.width = '1000px';
-main.style.height ='500px';
-main.style.position = 'relative';
-main.style.backgroundColor ="yellow";
-
-var mainBoxWidth = parseInt(main.style.width,10);
-var mainBoxHeight = parseInt(main.style.height,10);
-
 //random value generator
-var getRandomValue =function(upperLimit,lowerLimit){
-
-	return Math.random()*(upperLimit-lowerLimit);
+var getRandomValue = function( upperLimit , lowerLimit){
+  return Math.random() * (upperLimit-lowerLimit);
 };
 
-function Ant(antId){
-	this.width = ANT_WIDTH;
-	this.height = ANT_HEIGHT;
-	this.element=document.getElementById(antId);
-	this.velocity=getRandomValue(3,0);
-	this.y= getRandomValue(500,0);
-	this.x=getRandomValue(1000,0);
-	this.dx=getRandomValue(1,0);
-	this.dy=getRandomValue(1,0);	
+function Background (elementId){
+  this.mainElement = elementId;
+  this.ant = '';
+  this.ants = [];
 
-	var that = this;
+  var that = this;
 
-	this.collisionCheck =function(x){
+  this.create = function(){
 
-		if(this.x <= ants[x].x + ants[x].width &&
-			this.x + this.width >= ants[x].x &&
-			this.y <=ants[x].y + ants[x].height &&
-			this.height + this.y >= ants[x].y){
+    this.mainElement.style.width = '1000px';
+    this.mainElement.style.height ='500px';
+    this.mainElement.style.position = 'relative';
+    this.mainElement.style.backgroundColor ="yellow";
 
-			if(this.x>ants[x].x){
+    var mainBoxWidth = parseInt(this.mainElement.style.width,10);
+    var mainBoxHeight = parseInt(this.mainElement.style.height,10);
+    var bodies = document.getElementsByTagName("body");
+    var unOrderedList= document.createElement("ul");
 
-				this.dx=1;
-				ants[x].dx=-1;
+    unOrderedList.style.fontSize ="15px";
+    bodies[0].appendChild(unOrderedList);
 
-				if(this.y>ants[x].y){
+    var heading = document.createElement('h1');
+    var startButton=document.createElement('button');
 
-					this.dy=1;
-					ants[x].dy=-1;
-				}else{
+    this.mainElement.appendChild(heading);
+    this.mainElement.appendChild(startButton);
+    heading.style.color = 'red';
+    heading.appendChild(document.createTextNode('Start Game'));
+    startButton.appendChild(document.createTextNode('Start'));
 
-					ants[x].dy=1;
-					this.dy=-1;
-				}
-			}else{
+    //start on click
+    startButton.onclick = function(){
+      that.mainElement.removeChild(heading);
+      that.mainElement.removeChild(startButton);
 
-				ants[x].dx=1;
-				this.dx=-1;
+      for ( var x = 0; x < 10; x++ ){
+        that.ant = new Ant(this.mainElement);
+        that.ant.create();
+        that.ants.push(that.ant);
+      }
 
-				if(this.y>ants[x].y){
+      moveAnts=setInterval(function(){
+        for ( var x = 0; x <  that.ants.length; x++ ){
+          (function(antIndex){
+            antIndex.div.onclick = function(){
+              var clickedAnt = antIndex;
+              clickedAnt.antKill(clickedAnt.div);
+              that.ants.splice(that.ants.indexOf(clickedAnt), 1);  
 
-					this.dy=1;
-					ants[x].dy=-1;
-				}else{
-
-					ants[x].dy=1;
-					this.dy=-1;
-				}
-			}
-		}
-	}
-
-	this.changePosition =function(ants){
-
-		// small box coordinates
-		boxRightX=that.x+smallBoxWidth;
-		boxDownY=that.y+smallBoxWidth;
-
-		var avoidSearch = ants.indexOf(this);
-		// console.log('avodid'+avoidSearch);
-
-			//boxes collision
-			for(var y in ants){
-				for (var x in ants){
-
-					if(avoidSearch==x){
-					//don nothing;
-				}
-				else{
-
-					this.collisionCheck(x);
-				}		
-				
-			}
-		}
-
-		// boundary collision
-		if(boxRightX>=mainBoxWidth){
-
-			this.dx= -1;
-		}
-		else if(that.x<=0){
-
-			this.dx=1;
-		}
-		if(boxDownY>=mainBoxHeight){
-
-			this.dy= -1;
-		}
-		else if(that.y<=0){
-
-			this.dy='1';
-		}
-
-		this.x=this.x+this.velocity*this.dx;
-		this.y=this.y+this.velocity*this.dy;
-		this.element.style.left=this.x+"px";
-		this.element.style.top=this.y+"px";
-
-	}
-
-	this.antKill=function(){
-
-		this.element.parentNode.removeChild(this.element);
-
-		var top = this.element.style.top;
-		var left = this.element.style.left;	
-		var list = document.createElement("li");
-
-		list.setAttribute("class","positionList");
-		list.appendChild(document.createTextNode("left: "+left+" top: "+top));
+              if ( that.ants.length <=0){
+                clearInterval(moveAnts);    
+                that.resetGame();
+              }             
+            }
+          })(that.ants[x]);
 
 
-		unOrderedList.appendChild(list);
-	}
+          that.ants[x].collisionCheck(that.ants);
+          that.ants[x].update();
+        } 
 
-	this.resetGame=function(){
+      },10);
+    }
+  }
 
-		var finishScreen = document.createElement('h1');
-		var restartButton =document.createElement('button');
+  this.resetGame=function(){
 
-		finishScreen.style.color='red';
-		finishScreen.style.margin = '0px auto';
+    var finishScreen = document.createElement('h1');
+    var restartButton =document.createElement('button');
 
-		main.appendChild(finishScreen);
-		main.appendChild(restartButton);
+    finishScreen.style.color='red';
+    finishScreen.style.margin = '0px auto';
 
-		finishScreen.appendChild(document.createTextNode("Game Over"));
-		restartButton.appendChild(document.createTextNode("Restart"));
+    main.appendChild(finishScreen);
+    main.appendChild(restartButton);
 
-		restartButton.onclick=function(){
-			
-			createWorld();
-			finishScreen.parentNode.removeChild(finishScreen);
-			restartButton.parentNode.removeChild(restartButton);
+    finishScreen.appendChild(document.createTextNode("Game Over"));
+    restartButton.appendChild(document.createTextNode("Restart"));
 
-		}
-	}
+    restartButton.onclick=function(){
 
+     that.create();
+     finishScreen.parentNode.removeChild(finishScreen);
+     restartButton.parentNode.removeChild(restartButton);
+
+   }
+ }
 }
 
-var createWorld=function(){
 
-	for(var y=0; y<10;y++){	
+function Ant(parentElement){
+  this.mainElement = parentElement;
+  this.width = ANT_WIDTH;
+  this.height = ANT_HEIGHT;
+  this.div ='';
+  this.x=0;
+  this.y=0;
+  this.dx=0;
+  this.dy=0;
+  this.velocity=1;
 
-		var div=document.createElement("div");
+  var that = this;
 
-		div.style.width ='50px';
-		div.style.height ='50px';
-		div.style.backgroundColor ='black';
-		div.style.position = 'absolute';
-		div.setAttribute("id", "movingAnt"+y);
+  this.create=function(){
+    this.y = getRandomValue(500,0);
+    this.x = getRandomValue(1000,0);
+    this.dx = getRandomValue(1,0);
+    this.dy = getRandomValue(1,0);
+    this.velocity=getRandomValue(1,5);
+    this.div = document.createElement("div");
+    var that = this;
 
-		main.appendChild(div);
+    this.div.style.width ='50px';
+    this.div.style.height ='50px';
+    this.div.style.backgroundColor ='black';
+    this.div.style.position = 'absolute';
 
-		smallBoxWidth = parseInt(div.style.width,10);
+    main.appendChild(this.div);
 
-		var ant = new Ant("movingAnt"+y);	
+    smallBoxWidth = parseInt(this.div.style.width,10);
 
-		ants.push(ant);
+    this.div.style.top=this.y+'px';
+    this.div.style.left=this.x+'px';  
+  }
 
-	//console.log(ants.length);
+  this.update=function(){
+   // small box coordinates
+   boxRightX=this.x+smallBoxWidth;
+   boxDownY=this.y+smallBoxWidth;
+   // boundary collision
+   if(boxRightX>=MAX_BOX_WIDTH){
 
-	div.style.top=ant.y+'px';
-	div.style.left=ant.x+'px';
+     this.dx= 1;
+   }
+   else if(that.x<=0){
 
-	div.onclick =function(_ant){
+     this.dx=-1;
+   }
+   if(boxDownY>=MAX_BOX_HEIGHT){
 
-		return function(){
-			var clickedAnt = _ant;
-			
-			ants.splice(ants.indexOf(clickedAnt), 1);
+     this.dy= 1;
+   }
+   else if(that.y<=0){
 
-			if(ants.length<=0){
+     this.dy=-1;
+   }
 
-				clearInterval(moveAnts);	
-				ant.resetGame();
-			}
-			clickedAnt.antKill();
-		}			
-	}(ant);
+   this.x=this.x+this.velocity*this.dx;
+   this.y=this.y+this.velocity*this.dy;
+   this.div.style.left=this.x+"px";
+   this.div.style.top=this.y+"px"; 
+ }
 
+ this.collisionCheck =function(ants,x){
+
+  for(var x=0; x<ants.length;x++){
+
+    var avoidSearch = ants.indexOf(this);
+    //boxes collision
+    if ( avoidSearch == x ){
+      //don nothing;
+    }else{
+      if( this.x <= ants[x].x + ants[x].width &&
+       this.x + this.width >= ants[x].x &&
+       this.y <=ants[x].y + ants[x].height &&
+       this.height + this.y >= ants[x].y ){
+
+       if( this.x > ants[x].x ){
+
+         this.dx=-1;
+         ants[x].dx=1;
+
+         if(this.y > ants[x].y){
+
+           this.dy=-1;
+           ants[x].dy=1;
+         }else{
+
+           ants[x].dy=-1;
+           this.dy=1;
+         }
+       }else{
+
+         ants[x].dx=-1;
+         this.dx=1;
+
+         if(this.y>ants[x].y){
+
+           this.dy=-1;
+           ants[x].dy=1;
+         }else{
+
+           ants[x].dy=-1;
+           this.dy=1;
+         }
+       }
+     }
+   }
+ }
 }
 
-moveAnts=setInterval(function(){
+  this.antKill=function(killId){
+    killId.parentNode.removeChild(killId);
+  }
+}
 
-	for(var x=0;x<ants.length;x++){
+//start
+var newGame = new Background(main);
+newGame.create();
 
-		ants[x].changePosition(ants);
-	}
 
-},10);
-};
 
-createWorld();
+
+//  createWorld();
 
 
 
