f5ac2774c1c3b397f75068ff2f7081b36472ed86
yushghimire <yushghimire@gmail.com>
2017-11-28 07:04:30 +0545

    removing bike

--- dirt-bike/src/constants.js
+++
@@ -1,4 +0,0 @@
-const GRAVITY = 0.4;
-const FRICTION = 0.938;
-const AIR_FRICTION = 0.965;
-
--- dirt-bike/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	entry: "./src/app.js",
-	output: {
-		path:  __dirname,
-		filename: "./build/app.js"
-	}
-};
-
-
